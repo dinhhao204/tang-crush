8b04a781bfe610cbd828566430e728a56e7355de
dinhhao204 <94122519+dinhhao204@users.noreply.github.com>
2021-11-11 21:14:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    introTitle: 'Xin chao Babe - Bat Ngo Khum Nao',
+    introDesc: `Trúc Xinh Trúc Mọc Đầu Đình - Em Xinh Em Ngắm Mình Anh Mới Xinh ><`,
+    btnIntro: 'Yêu',
+    title: 'Nàng có yêu tôi không nào ^.^',
+    desc: 'Không Trả Lời Mà Thoát Ra Nghiệp Quật 3 Bữa Nhó - Phải Yêu Toi 3000 ',
+    btnYes: 'Đồng Ý Đồng Ý - Thêm Vào Giỏ Hàng ',
+    btnNo: 'Không Đồng Ý - Mơ Đi',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    btnReply: 'Chốt Đơn',
+    reply: 'Vì Cậu Xứng Đáng <33333333',
+    mess: 'Yêu Cậu 3000 <333333',
+    messDesc: 'Tối nay toi đón đi chill nhóoooooo ',
+    btnAccept: 'Chốt Đơn nhó <3',
+    messLink: 'https://www.messenger.com/t/100010810350944' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
